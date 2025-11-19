8423b88639401d420fcb478dca3961dc02a30436
Kaua Felix da Silva Costa <kfelixcosta11@gmail.com>
2025-11-19 13:58:51 -0300

    add: adiciona metodo para calcular o salario liquido de um funcionario

--- SRP/src/CalculadoraService.java
+++ SRP/src/CalculadoraService.java
@@ -1,9 +1,9 @@
 public class CalculadoraService {
     public double calcularSalarioLiquido(Funcionario funcionario){
         if (funcionario.getSalarioBase() > 5000.00) {
-            return funcionario.getSalarioBase() * 0.90; // 10% de imposto
+            return funcionario.getSalarioBase() * 0.90;
         } else {
-            return funcionario.getSalarioBase() * 0.95; // 5% de imposto
+            return funcionario.getSalarioBase() * 0.95;
         }
     }
 
