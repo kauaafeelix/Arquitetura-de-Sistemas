29c37bc1456d8e31474db46749ecc005a3c7c23f
Kaua Felix <kfelixcosta11@gmail.com>
2026-02-06 15:56:22 -0300

    add: adiciona D do solid

--- violacao_DIP/src/ServicoEmail.java
+++ violacao_DIP/src/ServicoEmail.java
@@ -1,7 +1,4 @@
 public class ServicoEmail implements NotificacaoService {
-    public void enviarEmail(String mensagem) {
-        System.out.println("Enviando E-mail SMTP: " + mensagem);
-    }
 
     @Override
     public void mensagem(String mensagem) {
